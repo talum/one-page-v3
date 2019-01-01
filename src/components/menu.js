46ee663fd236f6b2154e75eaf93bb32ec784c455
talum <tracy.a.lum@gmail.com>
2019-01-01 11:00:31 -0500

    wip dark theme

--- src/components/menu.js
+++ src/components/menu.js
@@ -12,7 +12,7 @@ const menuItems = [
 const Menu = ({ toggleMenu, open }) => (
   <menu id="js--menu" className="menu" data-open={open}>
     <div id="js--menu-close" className="menu__close" onClick={toggleMenu}>
-      <button className="button button--with-svg button--color-white button--small">
+      <button className="button button--with-svg button--color-contrast button--small">
         <svg viewBox="0 0 24 24" color="currentColor">
           <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z" />
         </svg>
@@ -21,7 +21,7 @@ const Menu = ({ toggleMenu, open }) => (
     <div className="menu__container">
       {menuItems.map(item => (
         <div key={item.title} className="menu__item">
-          <Link to={item.path} className="link--color-white">
+          <Link to={item.path} className="link--color-contrast">
             {item.title}
           </Link>
         </div>
