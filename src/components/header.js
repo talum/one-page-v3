29b5487bad81ee36516a87e049f4ab7b227cfd4d
talum <tracy.a.lum@gmail.com>
2019-01-14 23:43:19 -0500

    make search bar expand on click

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { navigate } from 'gatsby'
 import React from 'react'
 import qs from 'query-string'
 
-const Header = ({ siteTitle, toggleMenu }) => (
+const Header = ({ siteTitle, toggleMenu, toggleSearch, searchExpanded }) => (
   <header className="header">
     <div className="header__item">
       <button
@@ -17,36 +17,41 @@ const Header = ({ siteTitle, toggleMenu }) => (
       </button>
     </div>
     <div className="header__item">
-      <form
-        className="form-group"
-        name="searchform"
-        onSubmit={e => {
-          e.preventDefault()
-          navigate(`/search/?${qs.stringify({ q: e.target.firstChild.value })}`)
-        }}
-      >
-        <div className="form__search">
-          <div className="svg-container svg-container--small">
-            <svg viewBox="0 0 483.083 483.083">
-              <path
-                d="M332.74,315.35c30.883-33.433,50.15-78.2,50.15-127.5C382.89,84.433,298.74,0,195.04,0S7.19,84.433,7.19,187.85
-                  S91.34,375.7,195.04,375.7c42.217,0,81.033-13.883,112.483-37.4l139.683,139.683c3.4,3.4,7.65,5.1,11.9,5.1s8.783-1.7,11.9-5.1
-                  c6.517-6.517,6.517-17.283,0-24.083L332.74,315.35z M41.19,187.85C41.19,103.133,110.04,34,195.04,34
-                  c84.717,0,153.85,68.85,153.85,153.85S280.04,341.7,195.04,341.7S41.19,272.567,41.19,187.85z"
-              />
-            </svg>
-          </div>
-        </div>
-        <input
-          className="form__input"
-          name="search"
-          placeholder="Search"
-          type="text"
-        />
-      </form>
-    </div>
-    <div className="header__item">
       <div className="flex-container">
+        <div className="flex__item">
+          <form
+            className="form-group"
+            name="searchform"
+            onSubmit={e => {
+              e.preventDefault()
+              navigate(
+                `/search/?${qs.stringify({ q: e.target.lastChild.value })}`
+              )
+            }}
+          >
+            <div className="form__search" data-open={searchExpanded}>
+              <div
+                className="button button--with-svg button--small button--color-gray"
+                onClick={toggleSearch}
+              >
+                <svg viewBox="0 0 483.083 483.083">
+                  <path
+                    d="M332.74,315.35c30.883-33.433,50.15-78.2,50.15-127.5C382.89,84.433,298.74,0,195.04,0S7.19,84.433,7.19,187.85
+                      S91.34,375.7,195.04,375.7c42.217,0,81.033-13.883,112.483-37.4l139.683,139.683c3.4,3.4,7.65,5.1,11.9,5.1s8.783-1.7,11.9-5.1
+                      c6.517-6.517,6.517-17.283,0-24.083L332.74,315.35z M41.19,187.85C41.19,103.133,110.04,34,195.04,34
+                      c84.717,0,153.85,68.85,153.85,153.85S280.04,341.7,195.04,341.7S41.19,272.567,41.19,187.85z"
+                  />
+                </svg>
+              </div>
+            </div>
+            <input
+              className="form__input"
+              name="search"
+              placeholder="Search"
+              type="text"
+            />
+          </form>
+        </div>
         <div className="flex__item">
           <button className="button button--with-svg button--small button--with-svg-hover button--color-gray">
             <a
@@ -135,6 +140,8 @@ const Header = ({ siteTitle, toggleMenu }) => (
 Header.propTypes = {
   siteTitle: PropTypes.string,
   toggleMenu: PropTypes.func.isRequired,
+  toggleSearch: PropTypes.func.isRequired,
+  searchExpanded: PropTypes.bool.isRequired,
 }
 
 Header.defaultProps = {
